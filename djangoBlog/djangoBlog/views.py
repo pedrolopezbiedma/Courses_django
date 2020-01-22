5671b66897e110982118b05baf778713ebcca0bd
Pedro <pedrolopezbiedma@gmail.com>
2020-01-22 15:57:34 +0100

    Including templates on the root app

--- djangoBlog/djangoBlog/views.py
+++ djangoBlog/djangoBlog/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
-
-def handle_About_Page(request):
-    return HttpResponse('About page')
+#from django.http import HttpResponse
+from django.shortcuts import render 
 
 def handle_Home_Page(request):
-    return HttpResponse('Home page')
+    #return HttpResponse('About page')
+    return render(request, 'homepage.html')    
+
+
+def handle_About_Page(request):
+    #return HttpResponse('About page')
+    return render(request, 'about.html')
