e13b9d038a9f333c6ee689f71bfb376dc9f4bf94
Pedro <pedrolopezbiedma@gmail.com>
2020-01-23 09:32:26 +0100

    Creating our first model & migrating it

--- djangoBlog/articles/models.py
+++ djangoBlog/articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    # add author
+    # add thumbnail
