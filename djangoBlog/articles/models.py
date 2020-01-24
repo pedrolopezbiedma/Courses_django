e8059f3b409f9726f5cc8ae53b91e3460746d6f4
Pedro <pedrolopezbiedma@gmail.com>
2020-01-24 11:56:47 +0100

    Testing with the ORM & Admin site ( So cool )

--- djangoBlog/articles/models.py
+++ djangoBlog/articles/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Articles(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateField(auto_now_add=True)
     # add author
     # add thumbnail
+
+def __str__(self):
+    return self.title
