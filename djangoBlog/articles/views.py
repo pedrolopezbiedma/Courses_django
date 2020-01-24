e7a4f6ccba3ddfd8da993fdb70d8fa28ebf94d51
Pedro <pedrolopezbiedma@gmail.com>
2020-01-24 14:27:40 +0100

    Listing the articles in the database

--- djangoBlog/articles/views.py
+++ djangoBlog/articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
-
 def handle_Articles_List_Page(request):
-    return render(request, 'articles/articles-list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/articles-list.html', { 'articles': articles })
