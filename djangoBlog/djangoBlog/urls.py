cd224faf9170b3a19c81e9406f34b4381fe22f5f
Xnail <XnailDev@gmail.com>
2020-01-26 12:00:24 +0100

    Including static files within the project

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from . import views
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('about/', views.handle_About_Page),
     path('articles/', include('articles.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
