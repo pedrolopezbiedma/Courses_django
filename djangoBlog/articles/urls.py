6ff34ff3107bd5b2c5581e44e3eb722874703fc7
Xnail <XnailDev@gmail.com>
2020-01-26 15:39:17 +0100

    Including programatic routing

--- djangoBlog/articles/urls.py
+++ djangoBlog/articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.handle_Articles_List_Page),
+    path('', views.handle_Articles_List_Page, name='articleList'),
+    path('<slug>', views.handle_Articles_Detail_Page, name='articleDetails'),
 ]
--- djangoBlog/articles/views.py
+++ djangoBlog/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 # Create your views here.
 
 def handle_Articles_List_Page(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/articles-list.html', { 'articles': articles })
+
+def handle_Articles_Detail_Page(request, slug):
+    return HttpResponse(slug)
