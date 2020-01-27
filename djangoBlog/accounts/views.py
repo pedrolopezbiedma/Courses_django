d5858efb21a16f666dd951bd67e3339d2f013b69
Pedro <pedrolopezbiedma@gmail.com>
2020-01-27 10:47:48 +0100

    Including the users creation functionality

--- djangoBlog/accounts/views.py
+++ djangoBlog/accounts/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 # Create your views here.
 
 def handle_SignUp_Page(request):
-    form = UserCreationForm()
-    return render(request, 'accounts/signup.html', { 'form': form })
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Log the user
+            return redirect('articleList')
+        else:
+            return render(request, 'accounts/signup.html', {'form': form })
+            
+    elif request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form': form })
 
 def handle_Login_Page(request):
     return render(request, 'accounts/login.html')
