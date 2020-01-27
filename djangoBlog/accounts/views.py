bd7eb7ca057ce472044418667c1df543cab0faff
Pedro <pedrolopezbiedma@gmail.com>
2020-01-27 10:05:19 +0100

    Including the 'Signup' form ( not yet functionality )

--- djangoBlog/accounts/views.py
+++ djangoBlog/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 # Create your views here.
 
 def handle_SignUp_Page(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
 
 def handle_Login_Page(request):
     return render(request, 'accounts/login.html')
