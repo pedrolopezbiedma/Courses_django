c74240d52d7fe1d8a0245643921d64b34b51e03a
Pedro <pedrolopezbiedma@gmail.com>
2020-01-28 16:18:17 +0100

    Dynamically showing the 'Logout' or the 'Login + Register' links

--- djangoBlog/accounts/urls.py
+++ djangoBlog/accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.handle_SignUp_Page, name='signup'),
-    path('login/', views.handle_Login_Page, name='logIn'),
+    path('login/', views.handle_Login_Page, name='login'),
     path('logout/', views.handle_Logout_Page, name='logout')
 ]
