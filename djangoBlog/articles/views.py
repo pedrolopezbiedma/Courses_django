32563203a194646c6752fd2043e5b3a5ad8a3567
Pedro <pedrolopezbiedma@gmail.com>
2020-01-28 13:30:03 +0100

    Creating CreateArticle model form and using it within the Create Article template

--- djangoBlog/articles/views.py
+++ djangoBlog/articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 
 def handle_Articles_List_Page(request):
@@ -14,4 +15,14 @@ def handle_Articles_Detail_Page(request, slug):
 
 @login_required(login_url='/accounts/login/')
 def handle_Articles_Create_Page(request):
-    return render(request, 'articles/create-article.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid:
+            # Save to db
+            return redirect('articleList')
+        else:
+            return render(request, 'articles/create-article.html', {'form': form })
+
+    if request.method == 'GET':
+        form = forms.CreateArticle()
+        return render(request, 'articles/create-article.html', { 'form': form })
