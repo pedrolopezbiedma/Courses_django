d3abfc995a3e534df514cd9de1bfe2bb06f98ffe
Pedro <pedrolopezbiedma@gmail.com>
2020-01-28 11:55:13 +0100

    Include 'create article' page and protecting it from not logged users

--- djangoBlog/articles/urls.py
+++ djangoBlog/articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.handle_Articles_List_Page, name='articleList'),
+    path('create/', views.handle_Articles_Create_Page, name='createArticle'),
     path('<slug>', views.handle_Articles_Detail_Page, name='articleDetails'),
 ]
--- djangoBlog/articles/views.py
+++ djangoBlog/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def handle_Articles_List_Page(request):
@@ -10,3 +11,7 @@ def handle_Articles_List_Page(request):
 def handle_Articles_Detail_Page(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article-detail.html', { 'article': article })
+
+@login_required(login_url='/accounts/login/')
+def handle_Articles_Create_Page(request):
+    return render(request, 'articles/create-article.html')
